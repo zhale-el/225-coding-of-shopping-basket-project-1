a210ec37f72232939c0f3eb8210b6f0b5100884e
zhaleh <zhalehelmi@gmail.com>
2023-07-25 17:16:27 +0330

    daynamic-items

--- source/js/store.js
+++ source/js/store.js
@@ -6,3 +6,28 @@ let allProducts = [
   { id: 5, title: "Coffee", price: 98, img: "Images/Cofee.png" },
   { id: 6, title: "Shirt", price: 65.33, img: "Images/Shirt.png" },
 ];
+const shopItemsContainer = document.querySelector(".shop-items");
+
+allProducts.forEach(function (product) {
+  let productContainer = document.createElement("div");
+  productContainer.classList.add("shop-itme");
+
+  let productTitleSpan = document.createElement("span");
+  productTitleSpan.classList.add("shop-item-title");
+  productTitleSpan.innerHTML = product.title;
+
+  let productImageElem = document.createElement("img");
+  productImageElem.classList.add("shop-item-image");
+  productImageElem.setAttribute("src", product.img);
+
+  let productDetailContainer = document.createElement("div");
+  productDetailContainer.classList.add("shop-item-details");
+
+  let productPriceSpan = document.createElement("span");
+  productPriceSpan.innerHTML = product.price;
+  productPriceSpan.classList.add("shop-item-price");
+
+  let productAddBtn = document.createElement("button");
+  productAddBtn.innerHTML = "Add to cart";
+  productAddBtn.className = "btn btn-primary shop-item-button";
+});
